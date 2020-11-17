6b63342dd2bc7c423aaf92cf7d346e281ca5e01c
Karl Rapur <rapurkarl@gmail.com>
2020-11-17 21:06:47 +0200

    Implemented Post component

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,5 +1,25 @@
 <template>
-  
+    <div class="post">
+
+        <div class="post-author">
+            <span class="post-author-info">
+                <img src="${postOwner.avatar}" alt="Post author">
+                <small>{{postOwner.firstname}} {{postOwner.lastname}}</small>
+            </span>
+            <small>{{post.createTime}}</small>
+        </div>
+
+        <div class="post-image">
+            <img src="${postMedia.url}" alt="">
+            <!--
+                <video width="320" height="240" controls>` +
+                <source src="${postMedia.url}" type="video/mp4">Your browser does not support the video tag.</video>
+            -->
+        </div>
+
+        <div class="post-title"><h3>${post.text}</h3></div>
+        <div class="post-actions"><button type="button" name="like" class="like-button">${post.likes}</button></div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +29,86 @@ export default {
 </script>
 
 <style>
+.post {
+    width: 80%;
+    margin: 15px auto;
+    box-shadow: 0 0 15px rgba(38, 50, 56, 0.33);
+    border-radius: 5px;
+}
 
+.post .post-author {
+    padding: 10px;
+}
+
+.post .post-author::after {
+    content: "";
+    display: block;
+    clear: both;
+}
+
+.post .post-author .post-author-info {
+    float: left;
+    position: relative;
+    width: 50%;
+}
+
+.post .post-author .post-author-info img {
+    width: 30px;
+    height: 30px;
+    border-radius: 100%;
+    object-fit: cover;
+    object-position: top;
+    margin: 5px;
+}
+
+.post .post-author .post-author-info small {
+    position: absolute;
+    top: 10px;
+    left: 35px;
+}
+
+.post .post-author .post-author-info+small {
+    float: right;
+    color: grey;
+    padding: 10px;
+}
+
+.post .post-image img,
+video {
+    width: 100%;
+    min-height: 150px;
+    max-height: 350px;
+    object-fit: cover;
+    object-position: top center;
+}
+
+.post .post-title {
+    padding: 10px;
+}
+
+.post .post-title h3 {
+    display: inline;
+}
+
+.post .post-title~.post-actions {
+    padding: 10px;
+}
+
+.like-button {
+    background-image: url(../images/like.png);
+    background-size: 15px;
+    background-repeat: no-repeat;
+    background-position: 5px center;
+    background-color: #8a8a8a;
+    width: 60px;
+    height: 25px;
+    padding-left: 23px;
+    line-height: 10px;
+    text-align: left;
+    border: none;
+}
+
+.like-button.liked {
+    background-color: #01579b;
+}
 </style>
