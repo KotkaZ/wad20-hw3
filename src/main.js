87d32afa31e5add01fe93e2b56f39e60f7a9f4c3
Karl Rapur <rapurkarl@gmail.com>
2020-11-20 21:25:10 +0200

    Added Axios

--- src/main.js
+++ src/main.js
@@ -5,23 +5,23 @@ import VueRouter from 'vue-router'
 import Login from "./components/Login";
 import Browse from "./components/Browse";
 import Main from "./components/Main";
+import Axios from 'axios';
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 Vue.use(Vuex);
+Vue.use(Axios);
 
 const routes = [
-    {path: '/', component: Login},
-    {path: '/browse', component: Browse},
-    {path: '/main', component: Main},
+    { path: '/', component: Login },
+    { path: '/browse', component: Browse },
+    { path: '/main', component: Main },
 ];
 
-const router = new VueRouter({routes});
+const router = new VueRouter({ routes });
 
 
 new Vue({
     router,
     render: h => h(App)
 }).$mount('#app')
-
-
