42d0919488fac75f60441adaab041d63570a05ca
Karl Rapur <rapurkarl@gmail.com>
2020-11-22 17:08:14 +0200

    Visual fixes.

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,20 @@ export default {
 
 <style>
   * {
-    font-family: 'Open Sans', sans-serif;
+    font-family: 'Roboto Slab', serif;
+    outline: none;
+  }
+
+  html,
+  body {
+      margin: 0;
+      padding: 0;
+      width: 100%;
+      height: 100%;
+  }
+
+  a {
+    color: #40c4ff;
   }
 
   body {
